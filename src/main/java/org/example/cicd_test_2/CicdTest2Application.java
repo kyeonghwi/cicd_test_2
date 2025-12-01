6b3e7bd0747e8c965e351a239134e6750ebdc1ff
kyeonghwi <kyeonghwi@gmail.com>
2025-12-01 11:48:28 +0900

    first commit

---
+++ src/main/java/org/example/cicd_test_2/CicdTest2Application.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/cicd_test_2/CicdTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
