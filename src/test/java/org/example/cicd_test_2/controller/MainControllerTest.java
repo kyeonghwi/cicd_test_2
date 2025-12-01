88ba75d2dba3985170de2bd811fc0cc507e83183
kyeonghwi <kyeonghwi@gmail.com>
2025-12-01 12:26:42 +0900

    cicd test deploy

---
+++ src/test/java/org/example/cicd_test_2/controller/MainControllerTest.java
@@ -0,0 +1,16 @@
+package org.example.cicd_test_2.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainControllerTest {
+	// @Test
+	// void main() {
+	// 	MainController mc = new MainController();
+	// 	assertEquals(mc.main(null), "cicd test");
+	// }
+
+}
