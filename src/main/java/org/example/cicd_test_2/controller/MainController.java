98b1bb27eb764045ead7b996a57a7326868bf298
kyeonghwi <kyeonghwi@gmail.com>
2025-12-01 12:01:34 +0900

    cicd test

---
+++ src/main/java/org/example/cicd_test_2/controller/MainController.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test_2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MainController {
+
+	@GetMapping
+	public static String main(String[] args) {
+		return "main page";
+	}
+}
